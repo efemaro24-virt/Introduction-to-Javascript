fa843a4f1cda225728c75687ce6ec782c2c8e2fd
efemaro24-virt <efereuben@gmail.com>
2022-06-24 01:27:35 +0100

    A new Javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+let person = {
+    name : "Efe Reuben",
+    height :1.71,
+    country : "Nigeria"
+}
+ let output1 = document.getElementById("output1");
+ let output2 = document.getElementById("output2");
+ let output3 = document.getElementById("output3");
+ output1.textContent = "Hi, my name is : " + person.name 
+ output2.textContent = "My Height : " + person.height 
+ output3.textContent = "My Country : " + person.country
